9f0eff4d0d56a22c9cf7c8e6058e2d67b94e921a
efekos <hefeolgun10@gmail.com>
2024-03-18 22:46:41 +0300

    first commit

---
+++ src/main/java/dev/efekos/fancyhealthbar/FancyHealthBar.java
@@ -0,0 +1,10 @@
+package dev.efekos.fancyhealthbar;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FancyHealthBar implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dev/efekos/fancyhealthbar/client/FancyHealthBarClient.java
@@ -0,0 +1,10 @@
+package dev.efekos.fancyhealthbar.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FancyHealthBarClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
