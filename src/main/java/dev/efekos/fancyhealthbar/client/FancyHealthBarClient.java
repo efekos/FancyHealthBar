3680fee0c5c193b784ac0a153e84516c8dc66b42
efekos <hefeolgun10@gmail.com>
2024-03-18 23:17:21 +0300

    register the fancy health hud

--- src/main/java/dev/efekos/fancyhealthbar/client/FancyHealthBarClient.java
+++ src/main/java/dev/efekos/fancyhealthbar/client/FancyHealthBarClient.java
@@ -1,12 +1,15 @@
 package dev.efekos.fancyhealthbar.client;
 
+import dev.efekos.fancyhealthbar.client.hud.FancyHealthHud;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class FancyHealthBarClient implements ClientModInitializer {
+
+    public static final FancyHealthHud FANCY_HEALTH_HUD = new FancyHealthHud();
+
     @Override
     public void onInitializeClient() {
-
-
-
+        HudRenderCallback.EVENT.register(FANCY_HEALTH_HUD);
     }
 }
