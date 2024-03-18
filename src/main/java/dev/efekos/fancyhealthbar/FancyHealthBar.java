f66c3ae1b0aacf1eab9eb794d96e0b81cc21bcca
efekos <hefeolgun10@gmail.com>
2024-03-18 23:16:38 +0300

    add statements

--- src/main/java/dev/efekos/fancyhealthbar/FancyHealthBar.java
+++ src/main/java/dev/efekos/fancyhealthbar/FancyHealthBar.java
@@ -3,6 +3,8 @@ package dev.efekos.fancyhealthbar;
 import net.fabricmc.api.ModInitializer;
 
 public class FancyHealthBar implements ModInitializer {
+    public static final String MOD_ID = "fancyhealthbar";
+
     @Override
     public void onInitialize() {
 
--- src/main/java/dev/efekos/fancyhealthbar/client/FancyHealthBarClient.java
+++ src/main/java/dev/efekos/fancyhealthbar/client/FancyHealthBarClient.java
@@ -6,5 +6,7 @@ public class FancyHealthBarClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
+
+
     }
 }
