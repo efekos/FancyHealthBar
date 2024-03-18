788087fbe3e366e024abe13c7988f68d855ea1ce
efekos <hefeolgun10@gmail.com>
2024-03-18 23:16:50 +0300

    create a sample hud for testing

---
+++ src/main/java/dev/efekos/fancyhealthbar/client/hud/FancyHealthHud.java
@@ -0,0 +1,45 @@
+package dev.efekos.fancyhealthbar.client.hud;
+
+import dev.efekos.fancyhealthbar.client.object.HudObject;
+import dev.efekos.fancyhealthbar.client.object.PixelObject;
+import dev.efekos.fancyhealthbar.client.utils.HudLocation;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.gui.DrawContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class FancyHealthHud implements HudRenderCallback {
+
+    public static List<HudObject> OBJECTS = new ArrayList<>();
+
+    private static int gameTicks = 0;
+
+    @Override
+    public void onHudRender(DrawContext drawContext, float tickDelta) {
+
+        for (HudObject object : OBJECTS) {
+
+            if(gameTicks%20==0) object.tick();
+
+            if(object.getLocation().getX()>drawContext.getScaledWindowWidth()+5||object.getLocation().getY()>drawContext.getScaledWindowHeight()+5){
+                OBJECTS.remove(object);
+                continue;
+            }
+
+            object.draw(drawContext);
+        }
+
+        if(gameTicks%5==0) {
+
+            PixelObject object = new PixelObject();
+            object.setLocation(new HudLocation(drawContext.getScaledWindowWidth()/2, drawContext.getScaledWindowWidth()/2));
+            Random random = new Random();
+            object.addVelocity(random.nextInt(15)-7,random.nextInt(10));
+        }
+
+
+        gameTicks++;
+    }
+}
