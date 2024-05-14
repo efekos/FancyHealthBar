fc6064d1e1fd9a6e5a4677c396133fafd3938872
efekos <56752400+efekos@users.noreply.github.com>
2024-05-14 07:52:32 +0300

    change the mixin into an actual ClientPlayerEntityMixin

--- src/main/java/dev/efekos/fancyhealthbar/client/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/dev/efekos/fancyhealthbar/client/mixin/ClientPlayerEntityMixin.java
@@ -25,40 +25,37 @@
 package dev.efekos.fancyhealthbar.client.mixin;
 
 import dev.efekos.fancyhealthbar.client.FancyHealthBarClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PlayerEntity.class)
+@Mixin(ClientPlayerEntity.class)
 public abstract class ClientPlayerEntityMixin extends LivingEntity {
 
-    @Shadow
-    public abstract boolean isInvulnerableTo(DamageSource damageSource);
-
-    @Shadow
-    public abstract float getAbsorptionAmount();
+    @Shadow public abstract boolean isMainPlayer();
 
     public ClientPlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
 
-    @Inject(method = "applyDamage", at = @At("HEAD"))
-    public void onDamage(DamageSource damageSource, float damage, CallbackInfo ci) {
-        if (isInvulnerableTo(damageSource)) return;
+    @Inject(method = "damage", at = @At("HEAD"))
+    public void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if(!this.isMainPlayer()) return;
+        if (isInvulnerableTo(source)) return;
         if (getAbsorptionAmount() != 0) {
-            if (getAbsorptionAmount() - damage <= 0)
-                FancyHealthBarClient.FANCY_HEALTH_HUD.onDamage(MathHelper.clamp(getHealth(), 0, 20), MathHelper.clamp(getHealth() - (-(getAbsorptionAmount() - damage)), 0, 20));
+            if (getAbsorptionAmount() - amount <= 0)
+                FancyHealthBarClient.FANCY_HEALTH_HUD.onDamage(MathHelper.clamp(getHealth(), 0, 20), MathHelper.clamp(getHealth() - (-(getAbsorptionAmount() - amount)), 0, 20));
 
         } else
-            FancyHealthBarClient.FANCY_HEALTH_HUD.onDamage(MathHelper.clamp(getHealth(), 0, 20), MathHelper.clamp(getHealth() - damage, 0, 20));
+            FancyHealthBarClient.FANCY_HEALTH_HUD.onDamage(MathHelper.clamp(getHealth(), 0, 20), MathHelper.clamp(getHealth() - amount, 0, 20));
 
     }
 
