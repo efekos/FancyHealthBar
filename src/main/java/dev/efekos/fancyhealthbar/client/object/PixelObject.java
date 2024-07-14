5b9148d345e95ba5e6c106a1c8a8fcd901e8997c
efekos <56752400+efekos@users.noreply.github.com>
2024-07-14 17:21:45 +0300

    fix texture not stretching

--- src/main/java/dev/efekos/fancyhealthbar/client/object/PixelObject.java
+++ src/main/java/dev/efekos/fancyhealthbar/client/object/PixelObject.java
@@ -24,10 +24,15 @@
 
 package dev.efekos.fancyhealthbar.client.object;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import dev.efekos.fancyhealthbar.client.config.FancyHealthBarConfig;
 import dev.efekos.fancyhealthbar.client.utils.HudLocation;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.*;
 import net.minecraft.util.Identifier;
+import org.joml.Matrix4f;
+
+import java.lang.reflect.Field;
 
 public class PixelObject extends PhysicalHudObject {
 
@@ -54,11 +59,30 @@ public class PixelObject extends PhysicalHudObject {
 
     @Override
     public void draw(DrawContext context) {
-        //TODO fix texture not stretching
-        int x = getLocation().getX();
-        int y = getLocation().getY();
-        context.drawGuiTexture(texture, 9, 9, u, v, x, y, size, size);
+        int x1 = getLocation().getX();
+        int x2 = x1+size;
+        int y1 = getLocation().getY();
+        int y2 = y1+size;
+
+        drawTexture(context,texture,x1,x2,y1,y2,0,1,1,u,v,9,9);
     }
+
+    void drawTexture(DrawContext context,Identifier texture, int x1, int x2, int y1, int y2, int z, int regionWidth, int regionHeight, float u, float v, int textureWidth, int textureHeight) {
+        this.drawTexturedQuad(context,texture, x1, x2, y1, y2, z, (u + 0.0f) / (float)textureWidth, (u + (float)regionWidth) / (float)textureWidth, (v + 0.0f) / (float)textureHeight, (v + (float)regionHeight) / (float)textureHeight);
+    }
+
+    void drawTexturedQuad(DrawContext context,Identifier texture, int x1, int x2, int y1, int y2, int z, float u1, float u2, float v1, float v2) {
+        RenderSystem.setShaderTexture(0, texture);
+        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
+        Matrix4f matrix4f = context.getMatrices().peek().getPositionMatrix();
+        BufferBuilder bufferBuilder = Tessellator.getInstance().begin(VertexFormat.DrawMode.QUADS,VertexFormats.POSITION_TEXTURE);
+        bufferBuilder.vertex(matrix4f, x1, y1, z).texture(u1, v1);
+        bufferBuilder.vertex(matrix4f, x1, y2, z).texture(u1, v2);
+        bufferBuilder.vertex(matrix4f, x2, y2, z).texture(u2, v2);
+        bufferBuilder.vertex(matrix4f, x2, y1, z).texture(u2, v1);
+        BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
+    }
+
 
     @Override
     public int getGravity() {
